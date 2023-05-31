2335afe74d617146e63cc288e29f984f3e50fde2
Taarini1 <98458225+Taarini1@users.noreply.github.com>
2023-05-31 08:23:16 +0530

    Add files via upload

---
+++ Music_main.js
@@ -0,0 +1,199 @@
+function back(){
+    window.location="index.html"
+}
+function functionPause() {
+    document.getElementById("audio2").pause();
+    document.getElementById("audio3").pause();
+    document.getElementById("audio4").pause();
+    document.getElementById("audio5").pause();
+    document.getElementById("audio6").pause();
+    document.getElementById("audio7").pause();
+    document.getElementById("audio8").pause();
+    document.getElementById("audio9").pause();
+    document.getElementById("audio10").pause();
+    document.getElementById("audio11").pause();
+    document.getElementById("audio12").pause();
+    document.getElementById("audio13").pause();
+    document.getElementById("audio14").pause();
+}
+
+function function1() {
+    functionPause();
+    document.getElementById("audio1").play();
+  
+}
+function function2() {
+    document.getElementById("audio1").pause();
+}
+
+function function3() {
+    functionPause();
+    document.getElementById("audio1").play();
+}
+
+function function4() {
+    functionPause();
+    document.getElementById("audio2").play();
+}
+function function5() {
+    document.getElementById("audio2").pause();
+}
+
+function function6() {
+    functionPause();
+    document.getElementById("audio2").play();
+}
+
+function function7() {
+    functionPause();
+    document.getElementById("audio3").play();
+}
+function function8() {
+    document.getElementById("audio3").pause();
+}
+
+function function9() {
+    functionPause();
+    document.getElementById("audio3").play();
+}
+
+function function10() {
+    functionPause();
+    document.getElementById("audio4").play();
+}
+function function11() {
+    document.getElementById("audio4").pause();
+}
+
+function function12() {
+    functionPause();
+    document.getElementById("audio4").play();
+}
+
+function function13() {
+    functionPause();
+    document.getElementById("audio5").play();
+}
+function function14() {
+    document.getElementById("audio5").pause();
+}
+
+function function15() {
+    functionPause();
+    document.getElementById("audio5").play();
+}
+
+function function16() {
+    functionPause();
+    document.getElementById("audio6").play();
+}
+function function17() {
+    document.getElementById("audio6").pause();
+}
+
+function function18() {
+    functionPause();
+    document.getElementById("audio6").play();
+}
+
+function function19() {
+    functionPause();
+    document.getElementById("audio7").play();
+}
+function function20() {
+    document.getElementById("audio7").pause();
+}
+
+function function21() {
+    functionPause();
+    document.getElementById("audio7").play();
+}
+
+function function22() {
+    functionPause();
+    document.getElementById("audio8").play();
+}
+function function23() {
+    document.getElementById("audio8").pause();
+}
+
+function function24() {
+    functionPause();
+    document.getElementById("audio8").play();
+}
+function function25() {
+    functionPause();
+    document.getElementById("audio9").play();
+}
+function function26() {
+    document.getElementById("audio9").pause();
+}
+
+function function27() {
+    functionPause();
+    document.getElementById("audio9").play();
+}
+
+function function28() {
+    functionPause();
+    document.getElementById("audio10").play();
+}
+function function29() {
+    document.getElementById("audio10").pause();
+}
+
+function function30() {
+    functionPause();
+    document.getElementById("audio10").play();
+}
+
+
+function function31() {
+    functionPause();
+    document.getElementById("audio11").play();
+}
+function function32() {
+    document.getElementById("audio11").pause();
+}
+
+function function33() {
+    functionPause();
+    document.getElementById("audio11").play();
+}
+
+function function34() {
+    functionPause();
+    document.getElementById("audio12").play();
+}
+function function35() {
+    document.getElementById("audio12").pause();
+}
+
+function function36() {
+    functionPause();
+    document.getElementById("audio12").play();
+}
+
+function function37() {
+    functionPause();
+    document.getElementById("audio13").play();
+}
+function function38() {
+    document.getElementById("audio13").pause();
+}
+
+function function39() {
+    document.getElementById("audio13").play();
+}
+
+function function40() {
+    functionPause();
+    document.getElementById("audio14").play();
+}
+function function41() {
+    document.getElementById("audio14").pause();
+}
+
+function function42() {
+    document.getElementById("audio14").play();
+}
---
+++ main.js
@@ -0,0 +1,24 @@
+function Puzzles(){
+    window.location="puzzles.html";
+}
+
+function Riddles(){
+    window.location="riddles.html";
+}
+
+function Games(){
+    window.location="games.html";
+}
+
+function Songs(){
+    window.location="Music_index.html";
+}
+
+function Drawings(){
+    window.location="drawings.html";
+}
+
+function Contact(){
+    window.location="contact.html";
+}
+
